dd70844d75e27057826834067e650ca5e3e50c0b
Bagas <bagashyt@gmail.com>
2022-06-16 09:11:45 +0700

    add insertion sort

--- src/dartCode/bubble.dart
+++ src/dartCode/bubble.dart
@@ -1,7 +1,20 @@
 void main() {
   List<int> arr = [5, 3, -1, 1, 2, 1];
-  selection(arr);
+  insertion(arr);
   print(arr);
+}
+
+void insertion(List<int> arr){
+for(int i = 0; i < arr.length -1; i++){
+  for(int j = i+1; j> 0; j--){
+    if(arr[j] < arr[j-1]){
+      swap(arr, j, j-1);
+    }
+    else {
+      break;
+    }
+  }
+}
 }
 
 void selection(List<int> arr) {
--- src/javaCode/Bubble.java
+++ src/javaCode/Bubble.java
@@ -5,14 +5,26 @@ import java.util.Arrays;
 public class Bubble {
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 2};
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void selection(int[] arr){
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void selection(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // find the max item in the remaining array and swap with correct index
-            int last = arr.length -i -1;
+            int last = arr.length - i - 1;
             int maxIndex = getMaxIndex(arr, 0, last);
             swap(arr, maxIndex, last);
         }
@@ -26,9 +38,9 @@ public class Bubble {
 
 
     private static int getMaxIndex(int[] arr, int start, int end) {
-        int max =start;
-        for (int i = start; i <= end ; i++) {
-            if (arr[max] < arr[i]){
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
                 max = i;
             }
         }
@@ -43,11 +55,11 @@ public class Bubble {
             // for each step, max item will come at last respective index
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the item is smaller than the previous item
-                if (arr[j] < arr[j-1]) {
+                if (arr[j] < arr[j - 1]) {
                     //swap
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                     swapped = true;
                 }
             }
